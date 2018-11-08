a8096de5ad9f716aa2f1ce899e0bb70f3be47251
张志星 <2hang2hi8ing@gmail.com>
2018-11-08 16:39:51 +0800

    project add ribbon

--- spring-cloud-eureka-client-order/src/main/java/win/zhangzhixing/springcloudeurekaclientorder/service/impl/ProductOrderServiceImpl.java
+++ spring-cloud-eureka-client-order/src/main/java/win/zhangzhixing/springcloudeurekaclientorder/service/impl/ProductOrderServiceImpl.java
@@ -7,22 +7,34 @@ import win.zhangzhixing.springcloudeurekaclientorder.domain.ProductOrder;
 import win.zhangzhixing.springcloudeurekaclientorder.service.ProductOrderService;
 
 import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
 public class ProductOrderServiceImpl implements ProductOrderService {
-
+    // 调用方式一
     @Autowired
     private RestTemplate restTemplate;
+    // 调用方式二
+    // @Autowired
+    // private LoadBalancerClient loadBalancerClient;
 
     @Override
     public ProductOrder save(int userId, int productId) {
-        Object obj = restTemplate.getForObject("http://product-service/api/v1/product/find?id=" + productId, Object.class);
-        System.out.println(obj);
+        // 调用方式二
+        // ServiceInstance instance = loadBalancerClient.choose("product-service");
+        // String url = String.format("http://%s:%s/api/v1/product/find?id=" + productId, instance.getHost(), instance.getPort());
+        // RestTemplate restTemplate = new RestTemplate();
+        // Map<String, Object> productMap = restTemplate.getForObject(url, Map.class);
+        // 调用方式一
+        Map<String, Object> productMap = restTemplate.getForObject("http://product-service/api/v1/product/find?id=" + productId, Map.class);
+        System.out.println(productMap);
         ProductOrder productOrder = new ProductOrder();
         productOrder.setCreateTime(new Date());
         productOrder.setUserId(userId);
         productOrder.setTradeNo(UUID.randomUUID().toString());
+        productOrder.setProductName(productMap.get("name").toString());
+        productOrder.setPrice(Integer.parseInt(productMap.get("price").toString()));
         return productOrder;
     }
 }
